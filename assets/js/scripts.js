bc07c517f31732e9ae28ca93448df178daa62cd7
Tshegofatso Mailula <tshego.mailula@gmail.com>
2026-06-01 02:26:47 +0200

    Display results using unordered list

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,56 +6,115 @@ function submitMark(event)
     //Fetch input values
     let studentName = document.getElementById("student-name").value;
     let studentMark = document.getElementById("student-mark").value;
-    
+    let noResults = document.getElementById("no-results");
+    noResults.innerHTML = "";
     //Parse student mark
     studentMark = parseInt(studentMark);
-    let result = "FAIL";
-    let grade = "fail";
-    const student = {
-        name: studentName,
-        mark: studentMark
-    };
-    //Fetch and store students from localstorate
-    let students = JSON.parse(localStorage.getItem("students"));
-    if(!students)
-    {
-        students = [];
-    }
-    students.push(student);
-    students = JSON.stringify(students);
-    localStorage.setItem("students",students);
 
     //Validate input
     if(!studentName)
     {
         alert("Student name is required");
+        return;
     }
-    if(!studentMark || studentMark < 0 || student > 100)
+    if(!studentMark || studentMark < 0 || studentMark > 100)
     {
         alert("Invalid mark entered");
+        return;
     }
+    let result = "FAIL";
+    let grade = "fail";
+    const student_data = {
+        name: studentName,
+        mark: studentMark
+    };
 
-    //Check mark and determine grade
-    if(studentMark > 0 && studentMark <= 49)
+    //Fetch students from localstorate
+    let students = JSON.parse(localStorage.getItem("students"));
+    if(!students)
+    {
+        students = [];
+    }
+    students.push(student_data);
+
+    //Append table field
+    const table = document.getElementById("student-results");
+
+
+    const row = document.createElement("li");
+        //Check mark and determine grade
+    if(studentMark> 0 && studentMark<= 49)
     {
         result = "FAIL";
         grade = "Fail";
     }
-    else if(studentMark >= 50 && studentMark <= 64)
+    else if(studentMark>= 50 && studentMark<= 64)
     {
         result = "PASS";
         grade = "Pass";
     }
-    else if(studentMark >= 65 && studentMark <= 79)
+    else if(studentMark>= 65 && studentMark<= 79)
     {
         result = "PASS";
         grade = "Merit";
     }
-    else if(studentMark >= 80 && studentMark <= 100)
+    else if(studentMark>= 80 && studentMark<= 100)
     {
         result = "PASS";
         grade = "Distinction";
     }
+    row.innerHTML = `<span>${studentName}</span><span>${studentMark}</span><span>${result}</span><span>${grade}</span>`;
+    table.appendChild(row);
+    students = JSON.stringify(students);
+    
+    localStorage.setItem("students",students);
+    
     console.log(`The student name is ${studentName} and the student mark is ${studentMark} and the result is ${result} - ${grade}`);
-    console.log(`The database is ${students}`);
 }
+function loadStudents()
+{
+    let students = JSON.parse(localStorage.getItem("students"));
+    if(students)
+    {
+        
+        //Append table field
+        const table = document.getElementById("student-results");
+
+        students.forEach(student => {
+        const row = document.createElement("li");
+        let result = "FAIL";
+        let grade = "fail";
+            console.log(student);
+        //Check mark and determine grade
+        if(student.mark > 0 && student.mark <= 49)
+        {
+            result = "FAIL";
+            grade = "Fail";
+        }
+        else if(student.mark >= 50 && student.mark <= 64)
+        {
+            result = "PASS";
+            grade = "Pass";
+        }
+        else if(student.mark >= 65 && student.mark <= 79)
+        {
+            result = "PASS";
+            grade = "Merit";
+        }
+        else if(student.mark >= 80 && student.mark <= 100)
+        {
+            result = "PASS";
+            grade = "Distinction";
+        }
+        row.innerHTML = `<span>${student.name}</span><span>${student.mark}</span><span>${result}</span><span>${grade}</span>`;
+        table.appendChild(row);
+        });
+    }
+    if(!students)
+    {
+        let noResults = document.getElementById("no-results");
+        noResults.innerHTML = "No results available as yet";
+    }
+}
+//Load students on page load
+window.onload = loadStudents;
